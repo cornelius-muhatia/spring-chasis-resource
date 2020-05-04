7ad9bd77d364c6cde04de0c5113acf2781baf3d5
Cornelius Muhatia <muhatiacornelius@gmail.com>
2020-05-04 12:11:35 +0300

    Changed license header

--- src/main/java/com/cm/projects/spring/resource/chasis/wrappers/ResponseWrapper.java
+++ src/main/java/com/cm/projects/spring/resource/chasis/wrappers/ResponseWrapper.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2019 Cornelius M.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.cm.projects.spring.resource.chasis.wrappers;
@@ -105,7 +115,7 @@ public class ResponseWrapper <T> implements Serializable{
      * @return {@link ResponseWrapper}
      */
     public static <E> ResponseWrapper<E> ok(E data){
-        ResponseWrapper<E> r = new ResponseWrapper<E>();
+        ResponseWrapper<E> r = new ResponseWrapper<>();
         r.setData(data);
         r.setStatus(200);
         return r;
